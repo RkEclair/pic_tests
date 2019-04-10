2d45dd3bdc16476ecba7265816ccdf5d6f44af6e
RkEclair <ryosukadnak@gmail.com>
2019-04-10 21:51:04 +0900

    2A

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <xc.h>
 #include <stdio.h>
+#include <sys/attribs.h>
 
 #pragma config FPLLIDIV=DIV_2,FPLLMUL=MUL_20,FPLLODIV=DIV_2,FNOSC=FRCPLL,FSOSCEN=OFF,IESO=OFF,FPBDIV=DIV_1,FWDTEN=OFF,JTAGEN=OFF
 
-int _mon_getc(int canblock) {
-    while (!U1STAbits.URXDA);
-    return U1RXREG;
+unsigned char count_100ms = 0;
+
+void __ISR(_TIMER_1_VECTOR, IPL3SOFT) Timer1Handler() {
+    IFS0bits.T1IF = 0;
+    ++count_100ms;
 }
 
 void _mon_putc(char x) {
@@ -19,51 +22,26 @@ int main() {
     TRISB = 0b100;
     LATA = LATB = 0;
 
-    U1RXR = 4;
     RPB3R = 1;
 
     U1BRG = 1041;
-    U1STAbits.URXEN = 1;
     U1STAbits.UTXEN = 1;
     U1MODEbits.BRGH = 1;
     U1MODEbits.ON = 1;
 
-    unsigned char data;
+    PR1 = 12499;
+    T1CONbits.TCKPS = 1;
+    T1CONbits.ON = 1;
+    IPC1bits.T1IP = 3;
+    IEC0bits.T1IE = 1;
 
+    INTCONbits.MVEC = 1;
+    __builtin_enable_interrupts();
+
+    unsigned char count = 0, i = 10;
     while (1) {
-        scanf("%c", &data);
-        if (!('0' <= data && data <= '9')) continue;
-        switch (data - '0') {
-            case 0:
-                printf("");
-                break;
-            case 1:
-                printf("");
-                break;
-            case 2:
-                printf("");
-                break;
-            case 3:
-                printf("");
-                break;
-            case 4:
-                printf("");
-                break;
-            case 5:
-                printf("");
-                break;
-            case 6:
-                printf("");
-                break;
-            case 7:
-                printf("");
-                break;
-            case 8:
-                printf("");
-                break;
-            case 9:
-                printf("");
-                break;
-        }
+        while(count_100ms != 10); count_100ms = 0;
+        printf("%2d %#02x\r\n", count, count);
+        ++count;
     }
 }

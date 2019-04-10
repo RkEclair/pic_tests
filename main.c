fafc68acd4245a4b7da6459c27064a781ff2fcbc
RkEclair <ryosukadnak@gmail.com>
2019-04-10 16:49:08 +0900

    1B

--- main.c
+++ main.c
@@ -2,19 +2,29 @@
 
 #pragma config FPLLIDIV=DIV_2,FPLLMUL=MUL_20,FPLLODIV=DIV_2,FNOSC=FRCPLL,FSOSCEN=OFF,IESO=OFF,FPBDIV=DIV_1,FWDTEN=OFF,JTAGEN=OFF
 
+inline void delay_ms(unsigned char cyc) {
+    while (cyc-- != 0) {
+        while (!IFS0bits.T1IF);
+        IFS0bits.T1IF = 0;
+    }
+}
+
 int main() {
     ANSELA = ANSELB = 0;
-    TRISA = 0;
+    TRISA = 0b1000;
     TRISB = 0b100;
     LATA = LATB = 0;
-    
-    PR1 = 14999;
-    T1CONbits.TCKPS = 1;
+
+    PR1 = 999;
+    T1CONbits.TCKPS = 0;
     T1CONbits.ON = 1;
 
     while (1) {
-        while(!IFS0bits.T1IF);
+        while (!IFS0bits.T1IF);
         IFS0bits.T1IF = 0;
-        LATBbits.LATB4 ^= 1;
+        LATBbits.LATB4 = 1;
+        delay_ms(PORTAbits.RA3 ? 1 : 2);
+        LATBbits.LATB4 = 0;
+        delay_ms(15 - 1);
     }
 }

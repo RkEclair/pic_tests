24c82e7ef18c5884fd99c265cb404e10c56756e1
MX <ryosukadnak@gmail.com>
2019-05-08 15:35:08 +0900

    Again 1A

--- main.c
+++ main.c
@@ -1,49 +1,51 @@
 #include <xc.h>
-#include <stdio.h>
-#include <sys/attribs.h>
 
-#pragma config FPLLIDIV=DIV_2,FPLLMUL=MUL_20,FPLLODIV=DIV_2,FNOSC=FRCPLL,FSOSCEN=OFF,IESO=OFF,FPBDIV=DIV_1,FWDTEN=OFF,JTAGEN=OFF
+#pragma config FPLLIDIV=DIV_2
+#pragma config FPLLMUL=MUL_20
+#pragma config FPLLODIV=DIV_2
+#pragma config FNOSC=FRCPLL
+#pragma config FSOSCEN=OFF
+#pragma config IESO=OFF
+#pragma config FPBDIV=DIV_1
+#pragma config FWDTEN=OFF
+#pragma config JTAGEN=OFF
 
-void _mon_putc(char x) {
-    while (!U1STAbits.TRMT);
-    U1TXREG = x;
+inline void delay_ms(unsigned char cyc) {
+    while (cyc-- != 0) {
+        while (!IFS0bits.T1IF);
+        IFS0bits.T1IF = 0;
+    }
+}
+
+inline void setPR1(unsigned long us) {
+	if (us < 1598) {
+		PR1 = us * 40 - 1;
+		T1CONbits.TCKPS = 0;
+	} else if (us < 13102) {
+		PR1 = us * 5 - 1;
+		T1CONbits.TCKPS = 1;
+	} else if (us < 104856) {
+		PR1 = us * 40 / 64 - 1;
+		T1CONbits.TCKPS = 2;
+	} else {
+		PR1 = us * 40 / 256 - 1;
+		T1CONbits.TCKPS = 3;
+	}
+    T1CONbits.ON = 1;
 }
 
 int main() {
     ANSELA = 0;
-    ANSELB = 0x1000;
+		ANSELB = 0;
     TRISA = 0;
-    TRISB = 0x1004;
+    TRISB = 0b100;
     LATA = LATB = 0;
+    
+    setPR1(920);
 
-    PR1 = 12499;
-    T1CONbits.TCKPS = 1;
-    T1CONbits.ON = 1;
-    
-    AD1CHSbits.CH0SA = 12;
-    AD1CON3bits.ADCS = 3;
-    AD1CON1bits.ASAM = 1;
-    AD1CON1bits.ON = 1;
-    
-    unsigned sampled = 0;
-    const char type = '*', max_len = 10;
-    char i = 0;
-    
-    while(1) {
-        IFS0bits.T1IF = 0;
+    while (1) {
         while(!IFS0bits.T1IF);
-        
-        AD1CON1bits.SAMP = 0;
-        IFS0bits.AD1IF = 0;
-        while(!IFS0bits.AD1IF);
-        sampled = ADC1BUF0;
-
-        LATB = sampled << 2;
-        printf("%f", sampled * 0.004883);
-        char len = sampled * max_len / 1024.0;
-        for (i = 0; i < len; ++i) {
-            printf("%c", type);
-        }
-        printf("\r\n");
+        IFS0bits.T1IF = 0;
+        LATAbits.LATA2 ^= 1;
     }
 }

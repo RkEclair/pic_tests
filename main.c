9f43c1679bcac31003b233bbcac38a6317827b3e
MX <ryosukadnak@gmail.com>
2019-05-15 14:16:29 +0900

    Fix 1B

--- main.c
+++ main.c
@@ -10,44 +10,65 @@
 #pragma config FWDTEN=OFF
 #pragma config JTAGEN=OFF
 
-inline void delay_ms(unsigned char cyc) {
-    while (cyc-- != 0) {
-        while (!IFS0bits.T1IF);
-        IFS0bits.T1IF = 0;
-    }
-}
-
-inline void setPR1(unsigned long us) {
-	if (us < 1598) {
+inline void setPR1(unsigned long us)
+{
+	if (us < 1597) {
 		PR1 = us * 40 - 1;
 		T1CONbits.TCKPS = 0;
-	} else if (us < 13102) {
+	} else if (us < 13101) {
 		PR1 = us * 5 - 1;
 		T1CONbits.TCKPS = 1;
-	} else if (us < 104856) {
+	} else if (us < 104855) {
 		PR1 = us * 40 / 64 - 1;
 		T1CONbits.TCKPS = 2;
 	} else {
 		PR1 = us * 40 / 256 - 1;
 		T1CONbits.TCKPS = 3;
 	}
-    T1CONbits.ON = 1;
+	T1CONbits.ON = 1;
 }
 
-int main() {
-    ANSELA = ANSELB = 0;
-    TRISA = 0b1000;
-    TRISB = 0b100;
-    LATA = LATB = 0;
+inline void setPR2(unsigned long us)
+{
+	long test = 1638;
+	char pace = 0;
+	for (; pace < 7; ++pace) {
+		if (us < test) break;
+		us /= 2;
+		test *= 2;
+	}
+	PR2 = us * 40 - 1;
+	T2CONbits.TCKPS = pace;
+	T2CONbits.ON = 1;
+}
 
-    setPR1(1000);
+#define HAS_MASK(bits, val) ((bits & val) == val)
 
-    while (1) {
-        while (!IFS0bits.T1IF);
-        IFS0bits.T1IF = 0;
-        LATBbits.LATB4 = 1;
-        delay_ms(PORTAbits.RA3 ? 1 : 2);
-        LATBbits.LATB4 = 0;
-        delay_ms(15 - 1);
-    }
+int main()
+{
+	ANSELA = ANSELB = 0;
+	TRISA = 0b1000;
+	TRISB = 0b100;
+	LATA = LATB = 0;
+
+	setPR1(1000);
+	unsigned count = 0, count2 = 0;
+	char width = 1;
+	while (1) {
+		if (15 - count == 0) {
+			LATBbits.LATB4 = 1;
+			count = 0;
+		}
+		if (3000 <= count2) {
+			width = (width - 1 ? 1 : 2);
+			count2 = 0;
+		}
+		if (width - count == 0) {
+			LATBbits.LATB4 = 0;
+		}
+		while (!IFS0bits.T1IF);
+		IFS0bits.T1IF = 0;
+		++count;
+		++count2;
+	}
 }
